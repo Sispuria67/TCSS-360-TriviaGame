8d6921ae2d28c502790cbe4ca18612ca3a6f2e66
Rohit <rohitark34@gmail.com>
2024-06-03 02:13:59 -0700

    Created unit tests

---
+++ trivia/src/Tests/DoorTest.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import Model.Door;
+
+public class DoorTest {
+
+    private Door door;
+
+    @Before
+    public void setUp() {
+        door = new Door();
+    }
+
+    @Test
+    public void testInitialDoorStatus() {
+        assertEquals(Door.CLOSED, door.getDoorStatus());
+    }
+
+    @Test
+    public void testUnlock() {
+        door.unlock();
+        assertEquals(Door.OPEN, door.getDoorStatus());
+    }
+
+    @Test
+    public void testGetDoorIsLockedInitial() {
+        assertFalse(door.getDoorIsLocked());
+    }
+
+    @Test
+    public void testGetDoorIsLocked() {
+        door.setDoorStatus(Door.LOCKED);
+        assertTrue(door.getDoorIsLocked());
+    }
+
+    @Test
+    public void testSetDoorStatusToOpen() {
+        door.setDoorStatus(Door.OPEN);
+        assertEquals(Door.OPEN, door.getDoorStatus());
+    }
+
+    @Test
+    public void testSetDoorStatusToClosed() {
+        door.setDoorStatus(Door.CLOSED);
+        assertEquals(Door.CLOSED, door.getDoorStatus());
+    }
+
+    @Test
+    public void testSetDoorStatusToLocked() {
+        door.setDoorStatus(Door.LOCKED);
+        assertEquals(Door.LOCKED, door.getDoorStatus());
+    }
+}
---
+++ trivia/src/Tests/RoomTest.java
@@ -0,0 +1,55 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import Model.Room;
+
+public class RoomTest {
+
+    private Room room;
+
+    @Before
+    public void setUp() {
+        room = new Room();
+    }
+    @Test
+    public void testInitialDoorStates() {
+        assertFalse(room.getUpDoor());
+        assertFalse(room.getDownDoor());
+        assertFalse(room.getLeftDoor());
+        assertFalse(room.getRightDoor());
+    }
+
+    @Test
+    public void testSetUpDoor() {
+        room.setUpDoor(true);
+        assertTrue(room.getUpDoor());
+    }
+
+    @Test
+    public void testSetDownDoor() {
+        room.setDownDoor(true);
+        assertTrue(room.getDownDoor());
+    }
+
+    @Test
+    public void testSetLeftDoor() {
+        room.setLeftDoor(true);
+        assertTrue(room.getLeftDoor());
+    }
+
+    @Test
+    public void testSetRightDoor() {
+        room.setRightDoor(true);
+        assertTrue(room.getRightDoor());
+    }
+
+    @Test
+    public void testSetRoomName() {
+        String roomName = "Test Room";
+        room.setRoomName(roomName);
+        assertEquals(roomName, room.getRoomName());
+    }
+
+}
